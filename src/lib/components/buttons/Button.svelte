8df6d6044bdca0b5d2dd5d6c18018881a42184fe
FT111 <thetaylor2016@gmail.com>
2025-03-04 19:39:27 +0000

    Aesthetic tweaks

--- src/lib/components/buttons/Button.svelte
+++ src/lib/components/buttons/Button.svelte
@@ -17,7 +17,7 @@
 <Button.Root class="rounded-input text-black bg-muted w-48 h-48 peer
 											 shadow-mini hover:bg-muted/95 focus-visible:ring-foreground focus-visible:ring-offset-background focus-visible:outline-hidden
 				inline-flex h-12 items-center justify-center whitespace-nowrap px-[21px] text-[15px] font-semibold transition-colors focus-visible:ring-2
-				focus-visible:ring-offset-2 active:scale-[0.98] {className}"
+				focus-visible:ring-offset-2 active:scale-[0.98] hover:brightness-[1.02] transition-all {className}"
 	{...restProps}
 >
 	{@render children?.()}
